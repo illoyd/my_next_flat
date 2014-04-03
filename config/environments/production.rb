3fbdde31d31198a3a9787f0ca97b12c8288b52c4
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-04-03 09:04:38 +0100

    Add mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add GMail configuration for production server
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'signalcloudapp.com',
+    user_name:            Rails.application.secrets.gmail_username,
+    password:             Rails.application.secrets.gmail_password,
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
---
+++ config/initializers/devise_async.rb
@@ -0,0 +1,2 @@
+# config/initializers/devise_async.rb
+Devise::Async.backend = :sidekiq
