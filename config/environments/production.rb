748ae35339bdba398d60b96c214833e04c95815f
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-04-03 10:20:46 +0100

    Enable lograge

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Activate lograge
+  config.lograge.enabled = true
 
   # Add GMail configuration for production server
   config.action_mailer.delivery_method = :smtp
