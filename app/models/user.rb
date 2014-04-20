5e54dfd55055e3811a288cd57fb62dba27e18040
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-04-20 10:40:36 +0100

    Activate Twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,31 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [ :twitter ]
   has_many :searches, inverse_of: :user, dependent: :destroy
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.twitter_data"] && session["devise.twitter_data"]["info"]
+        user.email             = data["email"]    if user.email.blank?
+        user.name              = data['name']     if user.name.blank?
+        user.twitter_handle    = data["nickname"] if user.twitter_handle.blank?
+        user.profile_image_url = data['image']    if user.profile_image_url.blank?
+      end
+    end
+  end
+
+  def self.find_for_twitter_oauth(auth)
+    ( find_by(twitter_uid: auth.uid) || find_by(email: auth.info.email) || new ).tap do |user|
+      user.twitter_uid       = auth.uid
+      user.twitter_handle    = auth.info.nickname
+      user.email             = auth.info.email
+      user.password          ||= Devise.friendly_token[0,20]
+      user.name              = auth.info.name
+      user.profile_image_url = auth.info.image
+      user.save
+    end
+  end
+
 end
