b3d32725587c27f0c7ba7fad71b53a4db6bc1037
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-07-05 10:15:28 +0100

    New Example seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user1 = User.create!(name: Faker::Name.first_name, email: 'ian@signalcloudapp.com', password: 'test1234', password_confirmation: 'test1234')
+# user1 = User.create!(name: Faker::Name.first_name, email: 'ian@signalcloudapp.com', password: 'test1234', password_confirmation: 'test1234')
+
+# Precompile city-level searches
+[ 'London, England', 'Birmingham, England', 'Edinburgh, Scotland', 'Glasgow, Scotland', 'Cardiff City Centre, Wales', 'Bristol, England' ].each do |area|
+  Example.find_or_create_by(name: area) do |example|
+    example.locations << Location.new(area: area, radius: 2.0)
+    example.criterias << BuyCriteria.new
+    example.criterias << LetCriteria.new
+  end
+end
