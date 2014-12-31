e6765b355664b6bcdd15f874f84346803745f3cb
Ian Lloyd <ian.w.lloyd@gmail.com>
2014-12-31 10:10:26 +0000

    Update geocoder to use Telize and Cache

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,9 @@
-if ENV["REDISCLOUD_URL"] || ENV["REDIS_URL"]
+Geocoder.configure(
+  ip_lookup: :telize
+)
+
+if Redis.current || ENV["REDISCLOUD_URL"] || ENV["REDIS_URL"]
   Geocoder.configure(
     cache: Redis.current || Redis.new(:url => ENV["REDISCLOUD_URL"] || ENV["REDIS_URL"])
   )
 end
-
